6d546da6f141dadd2373984aeb387fc244f32cee
BroomyD <david.broomfield.uk@capgemini.com>
2026-05-15 15:11:26 +0000

    yyyyy

---
+++ octofit-tracker/frontend/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import Users from './components/Users';
+import Teams from './components/Teams';
+import Activities from './components/Activities';
+import Workouts from './components/Workouts';
+import Leaderboard from './components/Leaderboard';
+
+function App() {
+  return (
+    <div className="App container mt-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Octofit</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item"><NavLink className="nav-link" to="/users">Users</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/teams">Teams</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/activities">Activities</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/workouts">Workouts</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/leaderboard">Leaderboard</NavLink></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/users" element={<Users />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/" element={<div>Select a view from the navigation.</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
